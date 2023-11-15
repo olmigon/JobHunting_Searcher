4a65b6a46cb732c0a3b641ce5b10254bdf5412a8
shopippi-is-shopippi <52343354+shopippi@users.noreply.github.com>
2023-11-15 23:04:34 +0900

    Add files via upload

--- JH_Searcher.py
+++ JH_Searcher.py
@@ -11,23 +11,17 @@ def create_file():
     search_in_firefox(set_url)#urlを開く関数へ
 
 def search_in_firefox(url):
-    # FirefoxのWebDriverを初期化
     global driver
     driver = webdriver.Firefox()
 
     try:
-        # 指定されたURLを開く
         driver.get(url)
 
-        # ここで必要な処理を追加
-        global a
-        a=1
     finally:        
         print("This program is currently showing results for "+selected_text +" on openwork. ")
 
 # CTRL+Q を押したときに create_file() 関数を呼び出す
 keyboard.add_hotkey('ctrl+q', create_file)
 
-  # 仮のURL、実際のURLに置き換えてください
-# プログラムを実行し続ける
 keyboard.wait()
+#ソースコードを見ないで！文法めちゃくちゃかもしれない
